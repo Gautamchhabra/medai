e43ebd3dea27d5b22e3193ea74322eb34fb23926
gautamchhabra1607 <2220905.cse.coe@cgc.edu.in>
2026-06-27 19:28:02 +0530

    Configure PostgreSQL

--- medical_project/medical_project/settings.py
+++ medical_project/medical_project/settings.py
@@ -56,13 +56,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'medical_project.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
+}
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
